9a58ed445e0947b8b9ed9aec80e13d21ecb50d72
min <min@goldtek.com>
2014-06-11 17:20:11 +0800

    SPP Command

--- SppCommand/SPP_API.h
+++ SppCommand/SPP_API.h
@@ -1,3 +1,5 @@
+#include "windows.h"
+
 #ifndef __SPP_COMMAND_API_H__
 
 const TCHAR TAG[] = L"SppDebug";
@@ -11,11 +13,24 @@ BOOL SppCheckFile(char * szName);
 DWORD SppGetSize(TCHAR * szName);
 DWORD SppGetSize(char * szName);
 
-DWORD SppReadData(TCHAR * szName, DWORD pos, DWORD size);
-DWORD SppReadData(char * szName, DWORD pos, DWORD size);
+DWORD SppReadData(char * dest, TCHAR * szName, DWORD pos, DWORD size);
+DWORD SppReadData(char * dest, char * szName, DWORD pos, DWORD size);
 
-DWORD SppReadFile(TCHAR * destFile, TCHAR * logFile);
-DWORD SppReadFile(char * destFile, char * logFile);
+const char szCheckHead [] = "\
+sn = '%d' and year = '%d' and month = '%d' and day = '%d' and hour = '%d' and min = '%d' and sec = '%d' \
+and mode = '%c' and time = '%d' and distance = '%d'\r\n\
+";
+const char szSummary[] = "\
+'%d','%X','%d','%d','%d','%d','%d','%d','%c','%d','%d','%d','%d','%d','%d','%d','%d','%d','%d','%d','%d','%d','%d','%d','%d'\r\n\
+";
+const char szGps[] = "\
+'%d','%c','%d','%d','%d','%d','%d','%d','%f','%f','%d','%d','%d'\r\n\
+";
+
+TCHAR * SppReadFile(TCHAR * logFile);
+TCHAR * SppReadFile(char * logFile);
+
+DWORD GetProgress();
 
 //BOOL SppWriteFile(TCHAR szName, char * buf, DWORD size);
 //BOOL SppAppendFile(TCHAR szName, char * buf, DWORD size);
@@ -28,6 +43,7 @@ BOOL SppStopAll();
 class Dir {
 public:
 	friend Dir * SppDir();
+	friend Dir * SppDirSn();
 	Dir() {
 		buffer = NULL;
 		count = 0;
@@ -38,18 +54,33 @@ public:
 	void clear() {
 		if (buffer) {
 			delete[] buffer;
+			buffer = NULL;
 		}
 		count = 0;
 	}
 	DWORD GetCount() { return count; }
-	char* Get(DWORD index) { return buffer + 32 * index; }
-private:
-
+	char* Get(DWORD index) {
+		if(buffer && index < count) {
+			return buffer + 32 * index;
+		} else {
+			return NULL;
+		}
+	}
+	char* GetSub(DWORD index) {
+		if(buffer && index < count) {
+			return buffer+32 * index + 16;
+		} else {
+			return NULL;
+		}
+	}
 private:
 	DWORD count;
 	char * buffer;
 };
+
 Dir * SppDir();
+Dir * SppDirSn();
+
 /******************************************************************/
 
 TCHAR* SppGetVersion();
